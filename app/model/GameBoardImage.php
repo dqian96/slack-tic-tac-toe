307c400293e4a6f19bdbe9fff28d582d1e82a6f8
dqian <davidqian1996@gmail.com>
2016-07-13 05:00:49 -0400

    more big fixes

--- app/model/GameBoardImage.php
+++ app/model/GameBoardImage.php
@@ -29,7 +29,7 @@
 		public function outputImage() {
 			//header('Content-Type: image/png');
 			$savePath = $_SERVER['DOCUMENT_ROOT'] . '/slack-tic-tac-toe/public/images/' . $this->configs['boardImageName'];
-			$publicPath = $_SERVER['SERVER_NAME'] . '/public/images/' . $this->configs['boardImageName'];
+			$publicPath = __DIR__ . '/../../public/images/' . $this->configs['boardImageName'];
 			imagepng($this->boardResource, $savePath);
 			return $publicPath;
 		}
